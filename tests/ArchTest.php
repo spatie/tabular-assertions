f540f82a88bb93f3cb64d78345111be745bdd2e8
Freek Van der Herten <freek@spatie.be>
2023-12-29 23:46:09 +0100

    use arch function

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -1,6 +1,5 @@
 <?php
 
-test('it will not use debugging functions')
-    ->group('arch')
+arch('it will not use debugging functions')
     ->expect(['dd', 'dump', 'ray'])
     ->each->not->toBeUsed();
